d6d08228262048a67ab76a204592c922b8291f38
Dismal <1139730843@qq.com>
2017-09-26 17:42:55 +0800

    测试弹窗是否可用

--- app/view/patients/controller/patient-controller.js
+++ app/view/patients/controller/patient-controller.js
@@ -1,12 +1,17 @@
 class PatientCtrl {
-    constructor($rootScope, $scope, $state, APP_CONFIG, patientsService,$uibModal) {
+    constructor($rootScope, $scope, $state, APP_CONFIG, patientsService, $uibModal) {
         console.log($uibModal);
+        // var modals = $uibModal.open({
+        //     animation: true,
+        //     backdrop: 'static',
+        //     template: '<div>这是内容</div>'
+        // });
         patientsService.patients().then((data) => {
             console.log(data);
         });
     }
 }
-PatientCtrl.$inject = ['$rootScope', '$scope', '$state', 'APP_CONFIG', 'patientsService','$uibModal'];
+PatientCtrl.$inject = ['$rootScope', '$scope', '$state', 'APP_CONFIG', 'patientsService', '$uibModal'];
 module.exports = (ngMold) => {
     require.ensure(['../service/patients-service'], (require) => {
         require('../service/patients-service')(ngMold);
